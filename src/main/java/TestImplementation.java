8940451b78388312d2097d3c69eca241d18daba6
Hansamali Wijayatilake <rhansi.w@gmail.com>
2020-11-17 21:48:59 +0530

    Locate elements, complete signup functionality

--- src/main/java/TestImplementation.java
+++ src/main/java/TestImplementation.java
@@ -1,11 +1,15 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
 public class TestImplementation {
     WebDriver driver;
+
 
     @BeforeClass
     public void setup(){
@@ -16,6 +20,28 @@ public class TestImplementation {
 
     @Test
     public void signUp(){
+        // Locate Sign up button and store it for later use
+        WebElement signupButton = driver.findElement(By.xpath("//a[@href ='/user-registration']"));
+
+        // Click on the located Signup Button
+        signupButton.click();
+
+        // Verify current URL
+        String currentUrl = driver.getCurrentUrl();
+        String givenUrl = "https://www.testandquiz.com/user-registration";
+
+        System.out.println("Assert that the given URL is equal to the current URL");
+        Assert.assertEquals(currentUrl,givenUrl);
+
+    }
+
+    @Test
+    public void fillSignUpForm(){
+        // Locate the necessary text fields to fill the form
+
+
+
+
 
     }
 }
